37e47d041677d6ee057486433e9a933d17ae744f
Shisui <Shisui@SHISUI->
2022-12-12 02:05:34 +0200

    Добавьте файлы проекта.

---
+++ Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    internal class Controller
+    {
+        public async Task AddAsync(string msg)
+        {
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                Person tom = new Person(msg);
+                await JsonSerializer.SerializeAsync<Person>(fs, tom);
+                Console.WriteLine("Data has been saved to file");
+            }
+        }
+    }
+}
---
+++ DataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp13
+{
+    internal class DataBase
+    {
+
+        public static string GetAnsver(string question)
+        {
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                string str = question;
+                while (true)
+                {
+                    str = fs.ToString();
+                    if (str != null)
+                    {
+                        if (str == question)
+                            return " Есть такая заметка";
+                    }
+                    else
+                        return " Нет такой заметки";
+                }
+            }
+        }
+    }
+}
